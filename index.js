db48137c2bb70f451b1811a009323f28aa9b3697
Unknown <unknown@example.com>
2020-05-16 02:54:30 +0800

    feat: solution for problem 1
    add doAsync function as solution for problem 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // solutions here
+const { asyncOp, RandStream } = require('./lib/lib');
+//solution for problem 1
+const doAsync = (arrInput) => {
+    arrInput.reduce( (input) => {
+        if (typeof input === 'string') {
+			asyncOp(input);
+		} else {
+			input.map(asyncOp(input));
+		}
+	});
+}

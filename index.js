6548ce0e01ecb92dd9375b437547a85a8c98716d
Dan Ivan Sentillas <dan.ivan.sentilllas@gmail.com>
2020-05-17 11:40:34 +0800

    feat: solution to problem 2
    Add RandStringSource class that extends EventEmitter and emits data event when a string pattern is detected

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // solutions here
 const { asyncOp, RandStream } = require('./lib/lib');
+const events = require('events');
+
 //solution for problem 1
 const doAsync = (arrInput) => {
     arrInput.reduce( (input) => {
@@ -9,4 +11,21 @@ const doAsync = (arrInput) => {
 			input.map(asyncOp(input));
 		}
 	});
+}
+
+//solution for problem 2
+class RandStringSource extends events.EventEmitter
+{
+    constructor (randStream) {
+		this.randStream = randStream;
+		this.emitDataEvent();
+	}
+
+	emitDataEvent () {
+		let pattern = /\.(.*?)\./;
+		const arrMatch = pattern.exec(this.randStream);
+		if (arrMatch) {
+			this.emit('data', arrMatch[1]);
+		}
+	}
 }
